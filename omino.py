79427543198fc07c0a72c7941d10dd7831f959dd
Lewis Gaul <lewis.gaul@gmail.com>
2022-11-09 16:33:29 +0000

    Add README, formatting

--- omino.py
+++ omino.py
@@ -1,21 +1,22 @@
 from array import array
 
-class omino(object):
-    def __init__(self, size):
+
+class Omino:
+    def __init__(self, size: int):
         self.size = size
-        self.array = array("i", [1]*size*size)
-    
+        self.array = array("i", [1] * size * size)
+
     def __str__(self):
         ret = ""
         for x in range(0, self.size):
             for y in range(0, self.size):
-                if self.array[x*self.size + y] == 1:
+                if self.array[x * self.size + y] == 1:
                     ret += "#"
                 else:
                     ret += " "
             ret += "\n"
         return ret
 
+
 if __name__ == "__main__":
-    print(omino(4))
-    
+    print(Omino(4))
